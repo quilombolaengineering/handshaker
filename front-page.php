806ebf670e83c61d2012de86f6d6a4db1a8dc23b
munair <munair@qe.local>
2016-04-20 15:57:26 +0900

    implementing front-page.php

---
+++ front-page.php
@@ -0,0 +1,66 @@
+   <div class="slider"> 
+        <input type="radio" id="control1" name="controls" checked="checked"/>
+        <label for="control1"></label>
+        <input type="radio" id="control2" name="controls"/>
+        <label for="control2"></label>
+        <input type="radio" id="control3" name="controls"/>
+        <label for="control3"></label>
+        <input type="radio" id="control4" name="controls"/>
+        <label for="control4"></label>
+        <input type="radio" id="control5" name="controls"/>
+        <label for="control5"></label>
+        <div class="sliderinner">
+            <ul>
+                <li>
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/branch.jpg" />
+                    <div class="description">
+ 
+                        <div class="description-text">
+                            <h2>Branch</h2>
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/eraser-moss.jpg" />
+                    <div class="description">
+ 
+                        <div class="description-text">
+                            <h2>Slideshow Title 2</h2>
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/siren.jpg" />
+                    <div class="description">
+ 
+                        <div class="description-text">
+                            <h2>Slideshow Title 3</h2>
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/watercolor.jpg" />
+                    <div class="description">
+ 
+                        <div class="description-text">
+                            <h2>Slideshow Title 4</h2>
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/flowers.jpg" />
+                    <div class="description">
+ 
+                        <div class="description-text">
+                            <h2>Slideshow Title 5</h2>
+                            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div><!-- /.slider-->
