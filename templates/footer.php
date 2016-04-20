90b57476b7be4b7ec66bdcf7d05d19a850b60f29
munair <munair@qe.local>
2016-04-20 22:05:48 +0900

    basic slider working

--- templates/footer.php
+++ templates/footer.php
@@ -2,5 +2,4 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
-  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </footer>
