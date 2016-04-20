b36728e65a59aba60987400e9cdcfdf7e6c7578d
munair <munair@qe.local>
2016-04-20 18:43:19 +0900

    testing codepen example

--- front-page.php
+++ front-page.php
@@ -1,10 +1,148 @@
-<div class="container">
-  <img class='photo'  src="http://farm9.staticflickr.com/8320/8035372009_7075c719d9.jpg" alt="" />
-  <img class='photo'  src="http://farm9.staticflickr.com/8517/8562729616_35b1384aa1.jpg" alt="" />
-  <img class='photo'  src="http://farm9.staticflickr.com/8465/8113424031_72048dd887.jpg" alt="" />
-  <img class='photo'  src="http://farm9.staticflickr.com/8241/8562523343_9bb49b7b7b.jpg" alt="" />
+<h1>SASS Mixins for easy slider creation II</h1>
 
+<div class="fader-horizontal">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
 </div>
+
+
+<div class="fader-vertical">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
+
+
+<div class="slider-horizontal">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
+
+
+<div class="slider-vertical">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
+
+
+<div class="slider-vertical">
+  <ul>
+    <li>
+      <img src="http://placehold.it/800x200/ffffff" />
+    </li>
+    <li>
+       <img src="http://placehold.it/800x200" />
+    </li>
+    <li>
+      <img src="http://placehold.it/800x200/ffffff" />
+    </li>
+    <li>
+      <img src="http://placehold.it/800x200" />
+    </li>
+    <li>
+      <img src="http://placehold.it/800x200/ffffff" />
+    </li>
+  </ul>
+</div>
+
+
+
+
+<pre>
+// USAGE            
+@include vertical-slider(
+  $placeholder-name: slide-vertical,
+  $height: 200px,
+  $slides: 4,
+  $easing: ease-in-out,
+  $slide-duration: 5,
+  $trans-duration: 2
+);
+  
+.slider {
+  // extend generated placeholder                     
+  @extend %slide-vertical;   
+}
+</pre>
 
 <div class="slider"> 
         <input type="radio" id="control1" name="controls" checked="checked"/><label for="control1"></label>
--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,5 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </footer>
