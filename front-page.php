8efcdebeff77d729476a9308d9d389eb0ab6935e
munair <munair@qe.local>
2016-04-21 17:08:50 +0900

    rename div class because it might be ambiguous

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="slidercontainer">
   <img class='photo'  src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/watercolor.jpg" width="100%" alt="" />
   <img class='photo'  src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/siren.jpg"  width="100%"  alt="" />
   <img class='photo'  src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/eraser-moss.jpg" width="100%"  alt="" />
