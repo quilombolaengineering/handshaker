e5c957e9871fae6562bed9313a9a8dbfd83a3bd7
munair <munair@qe.local>
2016-04-21 16:39:24 +0900

    including entire vieron html

--- front-page.php
+++ front-page.php
@@ -5,7 +5,8 @@
   <img class='photo'  src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/branch.jpg"  width="100%"  alt="" />
 </div>
 
-<div class="fader-horizontal">
+<div class="page-content">
+      <div class="fader-horizontal">
   <ul>
     <li>
       <h2>YOUR CITY</h2>
@@ -26,6 +27,105 @@
     <li>
       <h2>YOUR CITY</h2>
       <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
+
+
+<div class="fader-vertical">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
+
+
+<div class="slider-horizontal">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
+
+
+<div class="slider-vertical">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
+
+
+<div class="slider-vertical">
+  <ul>
+    <li>
+      <img src="http://placehold.it/800x200/ffffff" />
+    </li>
+    <li>
+       <img src="http://placehold.it/800x200" />
+    </li>
+    <li>
+      <img src="http://placehold.it/800x200/ffffff" />
+    </li>
+    <li>
+      <img src="http://placehold.it/800x200" />
+    </li>
+    <li>
+      <img src="http://placehold.it/800x200/ffffff" />
     </li>
   </ul>
 </div>
