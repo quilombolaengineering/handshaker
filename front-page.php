c7e1c19ec18dc5dd697d741f7ee101032f39c679
munair <munair@qe.local>
2016-04-21 15:38:42 +0900

    add new html to front page

--- front-page.php
+++ front-page.php
@@ -4,3 +4,28 @@
   <img class='photo'  src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/eraser-moss.jpg" width="100%"  alt="" />
   <img class='photo'  src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/branch.jpg"  width="100%"  alt="" />
 </div>
+
+<div class="fader-horizontal">
+  <ul>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+    <li>
+      <h2>MADRID</h2>
+      <h3><em>#THETYPEINMADRID</em></h3>
+    </li>
+    <li>
+      <h2>BARCELONA</h2>
+      <h3><em>#THETYPEINBARCELONA</em></h3>
+    </li>
+    <li>
+      <h2>PANAMÁ</h2>
+      <h3><em>#THETYPEINPANAMA</em></h3>
+    </li>
+    <li>
+      <h2>YOUR CITY</h2>
+      <h3><em>#THETYPEINYOURCITY</em></h3>
+    </li>
+  </ul>
+</div>
