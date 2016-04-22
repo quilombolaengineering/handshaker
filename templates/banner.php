28fb0aea4c51d2baf4a91aaa2dcd07c95a0cda79
munair <munair@qe.local>
2016-04-22 21:12:02 +0900

    testing if bootstrap is actually loading buttons

--- templates/banner.php
+++ templates/banner.php
@@ -9,6 +9,7 @@
 		<h1><?php bloginfo('name'); ?></h1> 
 		<p><?php bloginfo('description'); ?></p>	
 		<button type="button">get involved</button>
+		<button type="button" class="btn btn-default">Default</button>
 		<a class="btn-white" href="#">Get Involved!</a>
 	</div>
 </section>
