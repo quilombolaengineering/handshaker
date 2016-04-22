6c17d5a540c26daf2e41a2c41e2be67b305419a7
munair <munair@qe.local>
2016-04-22 11:33:58 +0900

    simplifying the base frontpage to only include the banner

--- base-front-page.php
+++ base-front-page.php
@@ -16,23 +16,7 @@ use Roots\Sage\Wrapper;
     <![endif]-->
     <?php
       do_action('get_header');
-      get_template_part('templates/header');
-    ?>
-    <?php
-      do_action('get_header');
       get_template_part('templates/banner');
-    ?>
-    <div class="wrap container" role="document">
-      <div class="content row">
-        <main class="main">
-          <?php include Wrapper\template_path(); ?>
-        </main><!-- /.main -->
-      </div><!-- /.content -->
-    </div><!-- /.wrap -->
-    <?php
-      do_action('get_footer');
-      get_template_part('templates/footer');
-      wp_footer();
     ?>
   </body>
 </html>
