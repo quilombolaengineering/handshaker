37bc1059e6f8a2ece93acf2a717aa99f4a5ad8c8
munair <munair@qe.local>
2016-04-22 11:24:28 +0900

    update banner html

--- templates/banner.php
+++ templates/banner.php
@@ -1,15 +1,12 @@
-<!-- Sect: Banner
-======================================= -->
 <section class="sect-banner">
- <!--List items for bg images -->
- <ul class="kf-slider">
-  <li></li>
-  <li></li>
-  <li></li>
- </ul>
- <!-- Vertically Center Content -->
- <div class="v-center">
-  <h1>Stupid Simple Slider</h1>
-  <p>Silky smooth background image animations with css keyframes</p>
- </div>
+	<!-- Slider animation -->
+	<ul class="kf-slider">
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+	<div class="v-center">
+		<h1>Stupid Simple Slider</h1>
+		<p>Silky smooth background image slider with css keyframes</p>	
+	</div>
 </section>
