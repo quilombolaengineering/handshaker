b1a6f81039781a262b581270ec87258aee556229
munair <munair@qe.local>
2016-04-22 12:53:31 +0900

    revert

--- templates/banner.php
+++ templates/banner.php
@@ -1,6 +1,4 @@
 <section class="sect-banner">
-  <header class="landing-banner">
-    <div class="container">
 	<!-- Slider animation -->
 	<ul class="kf-slider">
 		<li></li>
@@ -11,6 +9,4 @@
 		<h1><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1> 
 		<p>Silky smooth background image slider with css keyframes</p>	
 	</div>
-    </div>
-  </header>
 </section>
