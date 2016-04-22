da4f372b564c3c63ce3e7ffcfc147103b4e0c546
munair <munair@qe.local>
2016-04-22 21:21:21 +0900

    testing buttons

--- templates/banner.php
+++ templates/banner.php
@@ -8,8 +8,6 @@
 	<div class="v-center">
 		<h1><?php bloginfo('name'); ?></h1> 
 		<p><?php bloginfo('description'); ?></p>	
-		<button type="button">get involved</button>
-		<button type="button" class="btn btn-default">Default</button>
-		<a class="btn-white" href="#">Get Involved!</a>
+		<a class="btn btn-default" href="#">Get Involved!</a>
 	</div>
 </section>
