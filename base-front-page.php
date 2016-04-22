38ae91cb759e8a1a94910ceec8634a313c35b73a
munair <munair@qe.local>
2016-04-22 11:41:41 +0900

    revert

--- base-front-page.php
+++ base-front-page.php
@@ -16,7 +16,23 @@ use Roots\Sage\Wrapper;
     <![endif]-->
     <?php
       do_action('get_header');
+      get_template_part('templates/header');
+    ?>
+    <?php
+      do_action('get_header');
       get_template_part('templates/banner');
+    ?>
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <main class="main">
+          <?php include Wrapper\template_path(); ?>
+        </main><!-- /.main -->
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+    <?php
+      do_action('get_footer');
+      get_template_part('templates/footer');
+      wp_footer();
     ?>
   </body>
 </html>
