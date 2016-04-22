27b95f0595a3515518534e01ac7c9e2aa84bc915
munair <munair@qe.local>
2016-04-22 12:50:27 +0900

    testing landing-banner

--- templates/banner.php
+++ templates/banner.php
@@ -1,4 +1,6 @@
 <section class="sect-banner">
+  <header class="landing-banner">
+    <div class="container">
 	<!-- Slider animation -->
 	<ul class="kf-slider">
 		<li></li>
@@ -6,7 +8,9 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-		<h1>Stupid Simple Slider</h1>
+		<h1><a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1> 
 		<p>Silky smooth background image slider with css keyframes</p>	
 	</div>
+    </div>
+  </header>
 </section>
