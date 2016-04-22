2678f1b497af1cbbe1be71f1a89d57cf8ff5daba
munair <munair@qe.local>
2016-04-23 08:36:58 +0900

    finish ribbon message

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -5,32 +5,32 @@
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#" hidefocus="true" style="outline: none;">
-						<h2>Become a<br>volunteer</h2>
+					<p><a href="#">
+						<h2>How to use<br>handshaker</h2>
 					</a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#" hidefocus="true" style="outline: none;">
-						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-1-80x80px.png"> <span>Become a<br>volunteer</span>
+					<p><a href="#">
+						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-1-80x80px.png"> <span>Activate<br>the theme</span>
 					</a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#" hidefocus="true" style="outline: none;">
-						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-2-80x80px.png"> <span>Become a<br>volunteer</span>
+					<p><a href="#">
+						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-2-80x80px.png"> <span>Use the<br>customizer</span>
 					</a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#" hidefocus="true" style="outline: none;">
-						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-3-80x80px.png"> <span>Become a<br>volunteer</span>
+					<p><a href="#">
+						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-3-80x80px.png"> <span>Start posting<br>content</span>
 					</a></p>
 				</div>
 			</div>
