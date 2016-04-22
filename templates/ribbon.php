2397cbbd7db74718a901a3b478b48f9d03d708b8
munair <munair@qe.local>
2016-04-23 08:58:41 +0900

    removing top and left positioning (test)

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -5,33 +5,25 @@
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#">
-						<h2>How to use<br>handshaker</h2>
-					</a></p>
+					<p><a href="#"> <h2>How can I use handshaker?</h2> </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#">
-						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-1-80x80px.png"> <span>Activate<br>the theme</span>
-					</a></p>
+					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-1-80x80px.png">Activate the theme </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#">
-						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-2-80x80px.png"> <span>Use the<br>customizer</span>
-					</a></p>
+					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-2-80x80px.png">Use the customizer </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
-					<p><a href="#">
-						<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-3-80x80px.png"> <span>Start posting<br>content</span>
-					</a></p>
+					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-3-80x80px.png">Start posting content </a></p>
 				</div>
 			</div>
 		</div>
