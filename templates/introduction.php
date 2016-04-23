85c4afd89485383324b30809d8810175484dc095
munair <munair@qe.local>
2016-04-23 14:04:57 +0900

    simplify divs

--- templates/introduction.php
+++ templates/introduction.php
@@ -4,9 +4,7 @@
 		<div class="row">
 			<!-- Introduction Image -->
 			<div class="col-md-6">
-				<div class="col-md-6-offset-3">
-					<img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
-				</div>
+				<img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
 			</div>
 			<!-- Introduction Text -->
 			<div class="col-md-6">
