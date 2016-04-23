bb20c035bbe4d1c108e3934dedcb252c48deb26f
munair <munair@qe.local>
2016-04-23 23:25:04 +0900

    remove rows and padding

--- templates/introduction.php
+++ templates/introduction.php
@@ -8,9 +8,9 @@
 			</div>
 			<!-- Introduction Text -->
 			<div class="col-md-6">
-				<div class="row introduction-title"><div class="col-xs-10 col-xs-offset-1"><h2>About Us</h2></div></div>
-				<div class="row introduction-line"><div class="col-xs-10 col-xs-offset-1"><div class="colored-line"></div></div></div>
-				<div class="row introduction-text"><div class="col-xs-10 col-xs-offset-1">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
+				<div class="col-xs-10 col-xs-offset-1 col-md-5 introduction-title"><h2>About Us</h2></div>
+				<div class="col-xs-10 col-xs-offset-1 col-md-5 introduction-line"><div class="colored-line"></div></div></div>
+				<div class="col-xs-10 col-xs-offset-1 col-md-5 introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
 				laboris nisi ut aliquip ex ea commodo consequat.</div></div>
