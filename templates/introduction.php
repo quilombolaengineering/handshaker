61b087443bbd3e696a55df4ecbbe66b651f27ff7
munair <munair@qe.local>
2016-04-23 11:54:25 +0900

    testing rows

--- templates/introduction.php
+++ templates/introduction.php
@@ -4,15 +4,15 @@
 		<div class="row">
 			<!-- Introduction Image -->
 			<div class="col-md-6">
-				<div class="introduction-image">
+				<div class="col-md-6-offset-3 introduction-image">
 					<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
 				</div>
 			</div>
 			<!-- Introduction Text -->
 			<div class="col-md-6">
-				<h2>About Us</h2>
-				<div class="colored-line-left"></div>
-				<div class="introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
+				<div class="row"><h2>About Us</h2></div>
+				<div class="row colored-line-left"></div>
+				<div class="row introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
 				laboris nisi ut aliquip ex ea commodo consequat.</div>
