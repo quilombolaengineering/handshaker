5bc1b92ca6bc351cf2f4d0c0be4a59ce3666ca9e
munair <munair@qe.local>
2016-04-24 03:49:01 +0900

    remove unnecessary divs from introduction

--- templates/introduction.php
+++ templates/introduction.php
@@ -9,11 +9,11 @@
 			<!-- Introduction Text -->
 			<div class="col-md-6">
 				<div class="col-xs-10 col-xs-offset-1 col-md-5 introduction-title"><h2>About Us</h2></div>
-				<div class="col-xs-10 col-xs-offset-1 col-md-5 introduction-line"><div class="colored-line"></div></div></div>
+				<div class="col-xs-10 col-xs-offset-1 col-md-5 introduction-line"><div class="colored-line"></div></div>
 				<div class="col-xs-10 col-xs-offset-1 col-md-5 introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-				laboris nisi ut aliquip ex ea commodo consequat.</div></div>
+				laboris nisi ut aliquip ex ea commodo consequat.</div>
 			</div>
 		</div>
 	</div>
