447785d91270e933d9e1872aa3a99d74fa071bd3
munair <munair@qe.local>
2016-04-23 18:40:06 +0900

    implement new divs

--- templates/introduction.php
+++ templates/introduction.php
@@ -8,12 +8,12 @@
 			</div>
 			<!-- Introduction Text -->
 			<div class="col-md-6">
-				<div class="row introduction-title"><h2>About Us</h2></div>
-				<div class="row"><div class="col-xs-6 col-offset-3 colored-line"></div></div>
-				<div class="row introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
+				<div class="row introduction-title"><div class="col-xs-10 col-xs-offset-1"><h2>About Us</h2></div></div>
+				<div class="row introduction-line"><div class="col-xs-10 col-xs-offset-1"></div></div>
+				<div class="row introduction-text"><div class="col-xs-10 col-xs-offset-1">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-				laboris nisi ut aliquip ex ea commodo consequat.</div>
+				laboris nisi ut aliquip ex ea commodo consequat.</div></div>
 			</div>
 		</div>
 	</div>
