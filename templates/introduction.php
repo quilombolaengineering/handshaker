df868a758b7984ece1f379a9f96d14c3388bd91f
munair <munair@qe.local>
2016-04-23 18:45:20 +0900

    add colored line

--- templates/introduction.php
+++ templates/introduction.php
@@ -9,7 +9,7 @@
 			<!-- Introduction Text -->
 			<div class="col-md-6">
 				<div class="row introduction-title"><div class="col-xs-10 col-xs-offset-1"><h2>About Us</h2></div></div>
-				<div class="row introduction-line"><div class="col-xs-10 col-xs-offset-1"></div></div>
+				<div class="row introduction-line"><div class="col-xs-10 col-xs-offset-1"><div class="colored-line"></div></div></div>
 				<div class="row introduction-text"><div class="col-xs-10 col-xs-offset-1">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
