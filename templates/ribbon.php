f7c5f29bb20a036409264b212ee539641abaa68c
munair <munair@qe.local>
2016-04-23 09:30:00 +0900

    remove overlay and size down ribbon-text h2

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -3,25 +3,21 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
-				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
 					<p><a href="#"> <h2>How can I use handshaker?</h2> </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
 					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-1-80x80px.png">Activate the theme </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
 					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-2-80x80px.png">Use the customizer </a></p>
 				</div>
 			</div>
 			<div class="col-md-3">
-				<div class="ribbon-overlay"></div>
 				<div class="ribbon-text">
 					<p><a href="#"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/ribbon-howto-3-80x80px.png">Start posting content </a></p>
 				</div>
