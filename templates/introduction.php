657e5fb33ede9ed9372e729b680d87f77d303698
munair <munair@qe.local>
2016-04-23 10:57:50 +0900

    breakout ribbon styling into its own partial and add introduction section

---
+++ templates/introduction.php
@@ -0,0 +1,22 @@
+<section class="section-introduction">
+	<!-- Introduce Organization -->
+	<div class="container">
+		<div class="row">
+			<!-- Introduction Image -->
+			<div class="col-md-6">
+				<div class="introduction-image">
+					<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
+				</div>
+			</div>
+			<!-- Introduction Text -->
+			<div class="col-md-6">
+				<h2>About Us</h2>
+				<div class="colored-line-left"></div>
+				<div class="introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+				laboris nisi ut aliquip ex ea commodo consequat.</div>
+			</div>
+		</div>
+	</div>
+</section>
