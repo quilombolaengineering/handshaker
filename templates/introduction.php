cf62633c993166ad8dac1fffe81a7697e823ca73
munair <munair@qe.local>
2016-04-25 12:35:19 +0900

    simplify... start with mobile first design.

--- templates/introduction.php
+++ templates/introduction.php
@@ -3,14 +3,13 @@
 	<div class="container-fluid organization-introduction">
 		<div class="row">
 			<!-- Introduction Image -->
-			<div class="col-md-6">
-				<img class="col-xs-10 col-xs-offset-1 introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
+			<div class="col-xs-10 col-xs-offset-2">
+				<img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
 			</div>
 			<!-- Introduction Text -->
-			<div class="col-md-6">
-				<div class="col-xs-10 col-xs-offset-1 introduction-title"><h2>About Us</h2></div>
-				<div class="col-xs-10 col-xs-offset-1 introduction-line"><div class="colored-line"></div></div>
-				<div class="col-xs-10 col-xs-offset-1 introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
+			<div class="col-xs-10 col-xs-offset-2 introduction-title"><h2>About Us</h2></div>
+			<div class="col-xs-10 col-xs-offset-2 introduction-line"><div class="colored-line"></div></div>
+			<div class="col-xs-10 col-xs-offset-2 introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
 				laboris nisi ut aliquip ex ea commodo consequat.</div>
