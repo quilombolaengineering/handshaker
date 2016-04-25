e5dc135eee83d6778e4daadc514924680abdf542
munair <munair@qe.local>
2016-04-25 13:32:13 +0900

    columns need to be 6 for medium length screens

--- templates/introduction.php
+++ templates/introduction.php
@@ -7,9 +7,9 @@
 				<img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
 			</div>
 			<!-- Introduction Text -->
-			<div class="col-xs-8 col-xs-offset-2 col-md-3 introduction-title"><h2>About Us</h2></div>
-			<div class="col-xs-8 col-xs-offset-2 col-md-3 introduction-line"><div class="colored-line"></div></div>
-			<div class="col-xs-8 col-xs-offset-2 col-md-3 introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
+			<div class="col-xs-8 col-xs-offset-2 col-md-6 introduction-title"><h2>About Us</h2></div>
+			<div class="col-xs-8 col-xs-offset-2 col-md-6 introduction-line"><div class="colored-line"></div></div>
+			<div class="col-xs-8 col-xs-offset-2 col-md-6 introduction-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. 
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
 				laboris nisi ut aliquip ex ea commodo consequat.</div>
