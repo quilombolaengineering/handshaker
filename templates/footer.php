a79899fd1110a0a73b785ec1cb3899487ae4e40d
munair <munair@qe.local>
2016-04-26 17:39:02 +0900

    initial footer formatting

--- templates/footer.php
+++ templates/footer.php
@@ -1,20 +1,28 @@
 <section class="section-footer">
-<h2>What We Do</h2>
-<p>Our Mission</p>
-<p>Get Involved</p>
-<p>Events</p>
-<p>Portfolio</p>
-<p>Blog</p>
-<h2>Who We Are</h2>
-<p>Our Team</p>
-<p>Partners</p>
-<p>Press</p>
-<p>Contact</p>
-<p>FAQ</p>
-<h2>About Us</h2>
-<p>Handshaker is a Wordpress Theme created by Quilombola Engineering. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-<h2>Links</h2>
-<p>Facebook</p>
+	<div class="col-md-3">
+		<h2>What We Do</h2>
+		<p>Our Mission</p>
+		<p>Get Involved</p>
+		<p>Events</p>
+		<p>Portfolio</p>
+		<p>Blog</p>
+	</div>
+	<div class="col-md-3">
+		<h2>Who We Are</h2>
+		<p>Our Team</p>
+		<p>Partners</p>
+		<p>Press</p>
+		<p>Contact</p>
+		<p>FAQ</p>
+	</div>
+	<div class="col-md-3">
+		<h2>About Us</h2>
+		<p>Handshaker is a Wordpress Theme created by Quilombola Engineering. Lonsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+	</div>
+	<div class="col-md-3">
+		<h2>Links</h2>
+		<p>Facebook</p>
+	</div>
 </section>
 
 <footer class="content-info">
