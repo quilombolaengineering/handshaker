f98cab9d6da637e93fb25bb6c2b08f72311c4303
munair <munair@qe.local>
2016-04-26 12:14:49 +0900

    remove unnecessary row

--- templates/artists.php
+++ templates/artists.php
@@ -22,8 +22,6 @@
 					<h1>Kai Qin</h1>
 					<p>Financing & Art Curation</p>
 				</div>
-			</div>
-			<div class="row">
 				<div class="col-xs-12 col-md-4">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
 					<h1>Soyoon</h1>
