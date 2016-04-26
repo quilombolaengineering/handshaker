d69bf8d0d374b3be389b3bd8a3bb0d4c91376fbf
munair <munair@qe.local>
2016-04-26 15:54:43 +0900

    change button transition effects.

--- templates/introduction.php
+++ templates/introduction.php
@@ -13,6 +13,7 @@
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut 
 				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
 				laboris nisi ut aliquip ex ea commodo consequat.</div>
+			<div class="col-xs-12 col-md-6 introduction-text"><div class="yellow-button">Read More</div></div>
 			</div>
 		</div>
 	</div>
