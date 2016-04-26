fd1bf98490955b0b4705baeb597fb89fe7ed991e
munair <munair@qe.local>
2016-04-26 15:24:31 +0900

    implement yellow button html

--- templates/banner.php
+++ templates/banner.php
@@ -9,5 +9,6 @@
 		<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" /></a>
 		<h1><?php bloginfo('name'); ?></h1> 
 		<p><?php bloginfo('description'); ?></p>	
+		<div class="yellow-button">Use Us ></div>
 	</div>
 </section>
