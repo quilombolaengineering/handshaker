92e73b88e6cbd429553b263bfbc6e6570c1ba7a8
munair <munair@qe.local>
2016-04-26 11:24:43 +0900

    implement bootstrap grid for artists section

--- templates/artists.php
+++ templates/artists.php
@@ -8,9 +8,20 @@
         <div class="v-center">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-xs-12"><a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a></div>
-                		<div class="col-xs-12"><h1>Zumbi</h1></div>
-                		<div class="col-xs-12"><p>Internet Marketing & Content Development</p></div>
+				<div class="col-xs-12 col-md-4">
+					<h1>Principal Artists</h1>
+					<h2>Handshake Founders</h2>
+				</div>
+                		<div class="col-xs-12 col-md-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
+					<h3>Zumbi</h3>
+					<p>Internet Marketing & Content Development</p>
+				</div>
+                		<div class="col-xs-12 col-md-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1200x1200px.jpeg" /></a>
+					<h3>Zumbi</h3>
+					<p>Internet Marketing & Content Development</p>
+				</div>
 			</div>
 		</div>
         </div>
