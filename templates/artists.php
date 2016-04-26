0958e84c5863449be9a7ba92cd987b6dcba6df31
munair <munair@qe.local>
2016-04-26 12:07:29 +0900

    add blurb

--- templates/artists.php
+++ templates/artists.php
@@ -8,15 +8,16 @@
         <div class="v-center">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-xs-12 col-md-4">
+				<div class="col-xs-12 title">
 					<h1>Principal Contributors</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				</div>
-                		<div class="col-xs-12 col-md-4">
+                		<div class="col-xs-12 col-md-6">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
 					<h1>Zumbi</h1>
 					<p>Marketing & Content Development</p>
 				</div>
-                		<div class="col-xs-12 col-md-4">
+                		<div class="col-xs-12 col-md-6">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1200x1200px.jpg" /></a>
 					<h1>Kai Qin</h1>
 					<p>Financing & Art Curation</p>
