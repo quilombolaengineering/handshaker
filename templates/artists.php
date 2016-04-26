e7b2a03d235b228177822e73189eebc27db234c5
munair <munair@qe.local>
2016-04-26 11:38:01 +0900

    add second row of artists

--- templates/artists.php
+++ templates/artists.php
@@ -23,6 +23,23 @@
 					<p>Internet Marketing & Content Development</p>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-xs-12 col-md-4 title">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
+					<h1>Zumbi</h1>
+					<p>Internet Marketing & Content Development</p>
+				</div>
+                		<div class="col-xs-12 col-md-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
+					<h1>Zumbi</h1>
+					<p>Internet Marketing & Content Development</p>
+				</div>
+                		<div class="col-xs-12 col-md-4">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1200x1200px.jpg" /></a>
+					<h1>Zumbi</h1>
+					<p>Internet Marketing & Content Development</p>
+				</div>
+			</div>
 		</div>
         </div>
 </section>
