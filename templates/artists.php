84d17c7e41b4fc0498dfd82163c42fadf7ca7afd
munair <munair@qe.local>
2016-04-26 11:53:58 +0900

    update contributors

--- templates/artists.php
+++ templates/artists.php
@@ -18,15 +18,15 @@
 				</div>
                 		<div class="col-xs-12 col-md-4">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1200x1200px.jpg" /></a>
-					<h1>Zumbi</h1>
-					<p>Internet Marketing & Content Development</p>
+					<h1>Kai Qin</h1>
+					<p>Financing & Art Curation</p>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-xs-12 col-md-4">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
-					<h1>Zumbi</h1>
-					<p>Internet Marketing & Content Development</p>
+					<h1>Soyoon</h1>
+					<p>Portfolio Management & Graphic Design</p>
 				</div>
                 		<div class="col-xs-12 col-md-4">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
@@ -36,7 +36,7 @@
                 		<div class="col-xs-12 col-md-4">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1200x1200px.jpg" /></a>
 					<h1>Zumbi</h1>
-					<p>Internet Marketing & Content Development</p>
+					<p>English Translation & Copy Editing</p>
 				</div>
 			</div>
 		</div>
