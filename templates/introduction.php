d7f950a0e2077353ff4dadbbf8ea36cf87fae83a
munair <munair@qe.local>
2016-04-26 18:08:07 +0900

    resize introduction story header... restyle it too.

--- templates/introduction.php
+++ templates/introduction.php
@@ -1,4 +1,4 @@
-<section class="section-introduction">
+<section id="section-introduction">
 	<!-- Introduce Organization -->
 	<div class="container-fluid organization-summary">
 		<div class="v-center">
@@ -20,7 +20,7 @@
 	<div class="container-fluid organization-story">
 		<div class="row">
 			<div class="col-xs-12">
-<h2>Introduction</h2>
+<h1>Introduction</h1>
 <p>This is a compelling story about Handshaker and its mission to change the world. In this section I will make you fall in love with Handshaker. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. </p>
 
 <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. </p>
