3099a7999f61729436a53b3aa12ba47c65c0ea46
munair <munair@qe.local>
2016-04-26 14:26:35 +0900

    add kai's photo

--- templates/artists.php
+++ templates/artists.php
@@ -18,7 +18,7 @@
 					<p>Marketing & Content Development</p>
 				</div>
                 		<div class="col-xs-12 col-md-6">
-					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1200x1200px.jpg" /></a>
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
 					<h1>Kai Qin</h1>
 					<p>Financing & Art Curation</p>
 				</div>
