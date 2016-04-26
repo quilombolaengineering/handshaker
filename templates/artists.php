e1ce89a5fdec83392a1abbf1e8ef913658d0d491
munair <munair@qe.local>
2016-04-26 11:56:43 +0900

    simplify styling

--- templates/artists.php
+++ templates/artists.php
@@ -8,8 +8,8 @@
         <div class="v-center">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-xs-12 col-md-4 title">
-					<p>Principal Contributors</p>
+				<div class="col-xs-12 col-md-4">
+					<h1>Principal Contributors</h1>
 				</div>
                 		<div class="col-xs-12 col-md-4">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
