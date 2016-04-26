975a07442b5baf70f3819d510d6c59ba9a26f6eb
munair <munair@qe.local>
2016-04-26 11:08:50 +0900

    implementing bootstrap grid for artists

--- templates/artists.php
+++ templates/artists.php
@@ -6,8 +6,12 @@
                 <li></li>
         </ul>
         <div class="v-center">
-		<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a>
-                <h1>Zumbi</h1>
-                <p>Internet Marketing & Content Development</p>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12"><a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/nosy-zumbi-1416x1416px.jpeg" /></a></div>
+                		<div class="col-xs-12"><h1>Zumbi</h1></div>
+                		<div class="col-xs-12"><p>Internet Marketing & Content Development</p></div>
+			</div>
+		</div>
         </div>
 </section>
