f675c972f7dfad598c3d1955953a1f2f10c8eeb2
munair <munair@qe.local>
2016-04-26 23:41:12 +0900

    update artist grid layout and implement some padding to intros

--- templates/artists.php
+++ templates/artists.php
@@ -12,14 +12,14 @@
 					<h1>Principal Contributors</h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				</div>
-                		<div class="col-xs-12 col-md-6">
+                		<div class="col-xs-12 col-md-4 col-md-offset-2">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/headshot-zumbi-441x441px.png" /></a>
 					<h1>Zumbi</h1>
 					<p>Marketing & Content Development</p>
 					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
-                		<div class="col-xs-12 col-md-6">
+                		<div class="col-xs-12 col-md-4">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
 					<h1>Kai Qin</h1>
 					<p>Financing & Art Curation</p>
