2b36a9cfe94b09c6f960e2022297c02840dd2c38
munair <munair@qe.local>
2016-04-27 16:13:14 +0900

    add appropriate class to the div

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -1,5 +1,5 @@
 <section id="section-sponsors">
-	<div class="container-fluid organization-story">
+	<div class="container-fluid sponsors">
 		<h1>Sponsors</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		<div class="row">
