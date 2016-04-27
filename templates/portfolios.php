fb0339082e999f95f93780590062449b0b70ebc0
munair <munair@qe.local>
2016-04-27 19:07:46 +0900

    add high def images

--- templates/portfolios.php
+++ templates/portfolios.php
@@ -7,9 +7,9 @@
 <p><b>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh</b>. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. <b>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</b>. <i>Quisque volutpat condimentum velit</i>. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. <i>Sed non quam</i>. </p>
 		</div>
 		<div class="row">
-			<div class="col-md-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/whiteheads-800x1200px.jpeg" width="100%"> </div>
+			<div class="col-md-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/mossheads-800x1200px.png" width="100%"> </div>
 			<div class="col-md-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/trapped-800x1200px.jpeg" width="100%"> </div>
-			<div class="col-md-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/whitesquare-800x1200px.jpeg" width="100%"> </div>
+			<div class="col-md-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/chubsters-1600x2400px.png" width="100%"> </div>
 			<div class="col-md-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/whiteheads-800x1200px.jpeg" width="100%"> </div>
 		</div>
 	</div>
