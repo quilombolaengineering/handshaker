180ad046a57ffa3e4eb4b9fcbaf160064aec775f
munair <munair@qe.local>
2016-04-27 15:59:34 +0900

    add sponsor blurb

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -1,5 +1,7 @@
 <section id="section-sponsors">
 	<div class="container-fluid organization-story">
+		<h1>Sponsors</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		<div class="row">
 			<div class="col-xs-2 col-xs-offset-3"> </div>
 			<div class="col-xs-2"> </div>
