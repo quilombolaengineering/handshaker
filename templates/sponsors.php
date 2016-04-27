0e7f27a5b1262f5d5b855364947a9bba53c770aa
munair <munair@qe.local>
2016-04-27 16:48:06 +0900

    change sponsor icons

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -3,8 +3,8 @@
 		<h1>Sponsors</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		<div class="row">
-			<div class="col-xs-2 col-xs-offset-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zumbcdo-inverted-icon-550x550px.png" width="80%"></div>
-			<div class="col-xs-2"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/capoeirastudio-icon-644x644px.png" width="80%"></div>
+			<div class="col-xs-2 col-xs-offset-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zumbcdo-square-icon-550x550px.png" width="80%"></div>
+			<div class="col-xs-2"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhang-simplified-aplha-logo.png" width="80%"></div>
 			<div class="col-xs-2"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/capoeirastudio-icon-644x644px.png" width="80%"></div>
 		</div>
 	</div>
