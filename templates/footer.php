8d88e35d861b8d1734e592acb9f9f122178b27fe
munair <munair@qe.local>
2016-04-28 10:50:32 +0900

    testing banner in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,38 +1,14 @@
-<section id="footer">
-<div class="container-fluid section-footer">
-<div class="v-center">
-	<div class="col-md-3">
-		<h2>What We Do</h2>
-		<p>Our Mission</p>
-		<p>Get Involved</p>
-		<p>Events</p>
-		<p>Portfolio</p>
-		<p>Blog</p>
+<section id="footer" class="section-banner">
+	<!-- slider animation -->
+	<ul class="keyframe-slider">
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+	<div class="v-center">
+		<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" /></a>
+		<h1><?php bloginfo('name'); ?></h1> 
+		<p><?php bloginfo('description'); ?></p>	
+		<div class="yellow-button">Usage</div>
 	</div>
-	<div class="col-md-3">
-		<h2>Who We Are</h2>
-		<p>Our Team</p>
-		<p>Partners</p>
-		<p>Press</p>
-		<p>Contact</p>
-		<p>FAQ</p>
-	</div>
-	<div class="col-md-3">
-		<h2>About Us</h2>
-		<p class="about-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. Lonsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. </p>
-	</div>
-	<div class="col-md-3">
-		<h2>Links</h2>
-		<p><i class="fa fa-facebook"></i></p>
-		<p><i class="fa fa-twitter"></i></p>
-		<p><i class="fa fa-instagram"></i></p>
-	</div>
-</div>
-</div>
 </section>
-
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-</footer>
