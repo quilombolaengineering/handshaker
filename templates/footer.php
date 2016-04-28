1d5954bec42f663137a03b8c7f1c3ee624c52498
munair <munair@qe.local>
2016-04-28 11:00:31 +0900

    swap in handshaker footer text

--- templates/footer.php
+++ templates/footer.php
@@ -6,9 +6,31 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-		<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" /></a>
-		<h1><?php bloginfo('name'); ?></h1> 
-		<p><?php bloginfo('description'); ?></p>	
-		<div class="yellow-button">Usage</div>
+        <div class="col-md-3">
+                <h2>What We Do</h2>
+                <p>Our Mission</p>
+                <p>Get Involved</p>
+                <p>Events</p>
+                <p>Portfolio</p>
+                <p>Blog</p>
+        </div>
+        <div class="col-md-3">
+                <h2>Who We Are</h2>
+                <p>Our Team</p>
+                <p>Partners</p>
+                <p>Press</p>
+                <p>Contact</p>
+                <p>FAQ</p>
+        </div>
+        <div class="col-md-3">
+                <h2>About Us</h2>
+                <p class="about-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. Lonsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna. </p>
+        </div>
+        <div class="col-md-3">
+                <h2>Links</h2>
+                <p><i class="fa fa-facebook"></i></p>
+                <p><i class="fa fa-twitter"></i></p>
+                <p><i class="fa fa-instagram"></i></p>
+        </div>
 	</div>
 </section>
