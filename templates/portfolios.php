b4e464bd7b9583966c9509068757789f7b1f4656
munair <munair@qe.local>
2016-04-28 16:13:38 +0900

    use lato 900 for project titles and descriptions

--- templates/portfolios.php
+++ templates/portfolios.php
@@ -5,22 +5,22 @@
 <p>This is a collection of art portfolios. Our art collection is divided into current, completed, planned, and award-wining projects. The <b>Decorated Projects</b> section below is a collection of art that has won awards. If you would like to collaborate with us as an <i>artist-in-residence</i>, please review <b>Upcoming Projects</b>. It is essentially a <i>call for entry / call for submission</i>. The curatorial plans submitted by artists like yourself are presented there. Art projects that we are presently working on are cataloged in <b>Current Projects</b>. Finally, all of our completed artwork has been categorized under <b>Completed Projects</b>.</p> 
 		</div>
 		<div class="row">
-			<div class="col-md-3"> 
+			<div class="col-md-3 projects"> 
 				Completed Projects
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/mossheads-800x1200px.png" width="100%">
 				What we have done.
 			</div>
-			<div class="col-md-3">
+			<div class="col-md-3 projects">
 				Current Projects
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/trapped-800x1200px.jpeg" width="100%"> 
 				What we are doing.
 			</div>
-			<div class="col-md-3"> 
+			<div class="col-md-3 projects"> 
 				Upcoming Projects
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/chubsters-1600x2400px.png" width="100%"> 
 				What we will do.
 			</div>
-			<div class="col-md-3">
+			<div class="col-md-3 projects">
 				Decorated Projects
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/whiteheads-800x1200px.jpeg" width="100%"> 
 				What we have won.
