fcc397cef5c42ad3d48814abde494d53cc341bf5
munair <munair@qe.local>
2016-04-28 18:56:05 +0900

    let's back this one out if it fails

--- templates/portfolios.php
+++ templates/portfolios.php
@@ -5,10 +5,10 @@
 <p>This is a collection of art portfolios. Our art collection is divided into current, completed, planned, and award-wining projects. The <b>Decorated Projects</b> section below is a collection of art that has won awards. If you would like to collaborate with us as an <i>artist-in-residence</i>, please review <b>Upcoming Projects</b>. It is essentially a <i>call for entry / call for submission</i>. The curatorial plans submitted by artists like yourself are presented there. Art projects that we are presently working on are cataloged in <b>Current Projects</b>. Finally, all of our completed artwork has been categorized under <b>Completed Projects</b>.</p> 
 		</div>
 		<div class="row">
-			<div class="col-md-3 projects"> 
+			<div class="col-md-3 projects project-overlay"> 
 				Completed Projects
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/mossheads-800x1200px.png" width="100%">
-				What we have done.
+				<div class="v-center"> <p> What we have done. </p> </div>
 			</div>
 			<div class="col-md-3 projects">
 				Current Projects
