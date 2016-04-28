b543dd272a5fc293ba93dedb6f7673332c277a41
munair <munair@qe.local>
2016-04-28 14:43:41 +0900

    use bootstrap included icons

--- templates/footer.php
+++ templates/footer.php
@@ -6,36 +6,32 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-        <div class="col-md-3">
-                <h2>What We Do</h2>
-                <p>Our Mission</p>
-                <p>Get Involved</p>
-                <p>Events</p>
-                <p>Portfolio</p>
-                <p>Blog</p>
-        </div>
-        <div class="col-md-3">
-                <h2>Who We Are</h2>
-                <p>Our Team</p>
-                <p>Partners</p>
-                <p>Press</p>
-                <p>Contact</p>
-                <p>FAQ</p>
-        </div>
-        <div class="col-md-3">
-                <h2>About Us</h2>
-                <p class="about-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. This theme was designed to showcase art portfolios for a team of artists living in China.</p>
-        </div>
-        <div class="col-md-3">
-                <h2>Links</h2>
-		<span class="social social-facebook"></span>
-		<span class="social social-twitter"></span>
-		<span class="social social-linked-in"></span>
-		<span class="social social-paypal"></span>
-		<span class="social social-skype"></span>
-		<span class="social social-instagram"></span>
-		<span class="social social-whatsapp"></span>
-		<span class="social social-e-mail-envelope"></span>
-        </div>
+	        <div class="col-md-3">
+	                <h2>What We Do</h2>
+	                <p>Our Mission</p>
+	                <p>Get Involved</p>
+	                <p>Events</p>
+	                <p>Portfolio</p>
+	                <p>Blog</p>
+	        </div>
+	        <div class="col-md-3">
+	                <h2>Who We Are</h2>
+	                <p>Our Team</p>
+	                <p>Partners</p>
+	                <p>Press</p>
+	                <p>Contact</p>
+	                <p>FAQ</p>
+	        </div>
+	        <div class="col-md-3">
+	                <h2>About Us</h2>
+	                <p class="about-text">Handshaker is a Wordpress Theme created by Quilombola Engineering. This theme was designed to showcase art portfolios for a team of artists living in China.</p>
+	        </div>
+	        <div class="col-md-3">
+                	<h2>Links</h2>
+			<div class="col-md-3"><span class="glyphicon glyphicon-search"></span>
+			<div class="col-md-3"><span class="glyphicon glyphicon-user"></span>
+			<div class="col-md-3"><span class="glyphicon glyphicon-leaf"></span>
+			<div class="col-md-3"><span class="glyphicon glyphicon-fire"></span>
+        	</div>
 	</div>
 </section>
