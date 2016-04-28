221a527a9c47efa880e01d7dcc5c86c5f87d5560
munair <munair@qe.local>
2016-04-28 10:23:43 +0900

    testing rubber band animation on sponsor images

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -3,9 +3,15 @@
 		<h1>Sponsors</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		<div class="row">
-			<div class="col-xs-2 col-xs-offset-3"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zumbcdo-square-icon-550x550px.png" width="80%"></div>
-			<div class="col-xs-2"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhang-simplified-aplha-logo.png" width="80%"></div>
-			<div class="col-xs-2"> <img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/capoeirastudio-icon-644x644px.png" width="80%"></div>
+			<div class="col-xs-2 col-xs-offset-3 col-md-4 col-md-offset-3"> 
+				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zumbcdo-square-icon-550x550px.png" class="sponsor-image">
+			</div>
+			<div class="col-xs-2 col-md-4"> 	
+				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhang-simplified-aplha-logo.png" class="sponsor-image">
+			</div>
+			<div class="col-xs-2 col-md-4"> 
+				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/capoeirastudio-icon-644x644px.png" class="sponsor-image">
+			</div>
 		</div>
 	</div>
 </section>
