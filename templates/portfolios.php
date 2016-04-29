5faa27e7b1e4e0baf6c9ea47f0c91951ee8b18dc
munair <munair@qe.local>
2016-04-30 08:43:23 +0900

    testing project title positioning

--- templates/portfolios.php
+++ templates/portfolios.php
@@ -5,10 +5,11 @@
 <p>This is a collection of art portfolios. Our art collection is divided into current, completed, planned, and award-wining projects. The <b>Decorated Projects</b> section below is a collection of art that has won awards. If you would like to collaborate with us as an <i>artist-in-residence</i>, please review <b>Upcoming Projects</b>. It is essentially a <i>call for entry / call for submission</i>. The curatorial plans submitted by artists like yourself are presented there. Art projects that we are presently working on are cataloged in <b>Current Projects</b>. Finally, all of our completed artwork has been categorized under <b>Completed Projects</b>.</p> 
 		</div>
 		<div class="row">
-			<div class="col-md-3 projects"> 
-				Completed Projects
+			<div class="col-md-3"> 
+				<div class="project">
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/mossheads-800x1200px.png" width="100%">
-				What we have done.
+				<div class="project-category">Completed Projects</div>
+				<div class="project-category-description">What we have done.</div>
 			</div>
 			<div class="col-md-3 projects">
 				Current Projects
