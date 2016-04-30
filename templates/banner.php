099ec9562cb262b3331401a2c443a9c5a8467c83
munair <munair@qe.local>
2016-05-01 00:19:52 +0900

    add sound to banner

--- templates/banner.php
+++ templates/banner.php
@@ -6,7 +6,9 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-		<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" /></a>
+		<a onmousedown="tick()" onmouseover="pop()" href="#ribbon">
+			<img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" />
+		</a>
 		<h1><?php bloginfo('name'); ?></h1> 
 		<p><?php bloginfo('description'); ?></p>	
 		<div class="yellow-button">Usage</div>
