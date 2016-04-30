1c6bb36999aaa6e3e146827f2c82f6de944838bb
munair <munair@qe.local>
2016-05-01 00:29:45 +0900

    revert to a element

--- templates/banner.php
+++ templates/banner.php
@@ -6,9 +6,9 @@
 		<li></li>
 	</ul>
 	<div class="v-center">
-		<div onmousedown="tick()" onmouseover="pop()"><a href="#ribbon">
+		<a onmousedown="tick()" onmouseover="pop()" href="#ribbon">
 			<img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" />
-		</a></div>
+		</a>
 		<h1><?php bloginfo('name'); ?></h1> 
 		<p><?php bloginfo('description'); ?></p>	
 		<div class="yellow-button">Usage</div>
