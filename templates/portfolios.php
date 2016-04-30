4dfe00d406bd72a7a3d1edcef43ca08bc70f4ce9
munair <munair@qe.local>
2016-04-30 17:18:32 +0900

    place projects in between text

--- templates/portfolios.php
+++ templates/portfolios.php
@@ -2,7 +2,7 @@
 	<div class="container-fluid portfolios">
 		<div class="row">
 <h1>Art Portfolios</h1>
-<p>This is a collection of art portfolios. Our art collection is divided into current, completed, planned, and award-wining projects. The <b>Decorated Projects</b> section below is a collection of art that has won awards. If you would like to collaborate with us as an <i>artist-in-residence</i>, please review <b>Upcoming Projects</b>. It is essentially a <i>call for entry / call for submission</i>. The curatorial plans submitted by artists like yourself are presented there. Art projects that we are presently working on are cataloged in <b>Current Projects</b>. Finally, all of our completed artwork has been categorized under <b>Completed Projects</b>.</p> 
+<p>This is a collection of art portfolios. Our art collection is divided into current, completed, planned, and award-wining projects.</p>
 		</div>
 		<div class="row projects">
 			<div class="col-md-3 completed-projects"> 
@@ -21,6 +21,9 @@
 				<h1>Decorated Projects</h1>
 				<p>What we have won.</p>
 			</div>
+		</div>
+		<div class="row">
+<p> The <b>Decorated Projects</b> section above is a collection of art that has won awards. If you would like to collaborate with us as an <i>artist-in-residence</i>, please review <b>Upcoming Projects</b>. It is essentially a <i>call for entry / call for submission</i>. The curatorial plans submitted by artists like yourself are presented there. Art projects that we are presently working on are cataloged in <b>Current Projects</b>. Finally, all of our completed artwork has been categorized under <b>Completed Projects</b>.</p> 
 		</div>
 	</div>
 </section>
