6fad88fe0b2cd47725c417028b1fbd2c20ca5baf
munair <munair@qe.local>
2016-04-30 11:22:36 +0900

    testing portfolios with image in the background

--- templates/portfolios.php
+++ templates/portfolios.php
@@ -5,29 +5,30 @@
 <p>This is a collection of art portfolios. Our art collection is divided into current, completed, planned, and award-wining projects. The <b>Decorated Projects</b> section below is a collection of art that has won awards. If you would like to collaborate with us as an <i>artist-in-residence</i>, please review <b>Upcoming Projects</b>. It is essentially a <i>call for entry / call for submission</i>. The curatorial plans submitted by artists like yourself are presented there. Art projects that we are presently working on are cataloged in <b>Current Projects</b>. Finally, all of our completed artwork has been categorized under <b>Completed Projects</b>.</p> 
 		</div>
 		<div class="row">
-			<div class="col-md-3"> 
-				<div class="project">
-				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/mossheads-800x1200px.png" width="100%">
-				<div class="project-category">Completed Projects</div>
-				<div class="project-category-description">What we have done.</div>
+			<div class="col-md-3 current-projects"> 
+				<h1>Completed Projects</h1>
+				<p>What we have done.</p>
 			</div>
 			<div class="col-md-3">
 				<div class="project">
-				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/trapped-800x1200px.jpeg" width="100%"> 
-				<div class="project-category">Current Projects</div>
-				<div class="project-category-description">What we are doing.</div>
+					<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/trapped-800x1200px.jpeg" width="100%"> 
+					<div class="project-category">Current Projects</div>
+					<div class="project-category-description">What we are doing.</div>
+				</div>
 			</div>
 			<div class="col-md-3"> 
 				<div class="project">
-				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/chubsters-1600x2400px.png" width="100%"> 
-				<div class="project-category">Upcoming Projects</div>
-				<div class="project-category-description">What we will do.</div>
+					<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/chubsters-1600x2400px.png" width="100%"> 
+					<div class="project-category">Upcoming Projects</div>
+					<div class="project-category-description">What we will do.</div>
+				</div>
 			</div>
 			<div class="col-md-3">
 				<div class="project">
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/whiteheads-800x1200px.jpeg" width="100%"> 
 				<div class="project-category">Decorated Projects</div>
 				<div class="project-category-description">What we have won.</div>
+				</div>
 			</div>
 		</div>
 	</div>
