e13c8d5407ebdb309685cb33905ce535e144ceca
munair <munair@qe.local>
2016-05-02 17:36:49 +0900

    oops

--- templates/sponsors.php
+++ templates/sponsors.php
@@ -3,7 +3,7 @@
 		<h1>Sponsors</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		<div class="row">
-			<div class="col-xs-4 col-md-2 col-xs-offset-3"> 
+			<div class="col-xs-4 col-md-2 col-md-offset-3"> 
 				<img src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zumbcdo-square-icon-550x550px.png">
 			</div>
 			<div class="col-xs-4 col-md-2"> 	
