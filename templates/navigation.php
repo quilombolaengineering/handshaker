30fb9f22a09527a2fc8e0a9e12913366bacfd9f1
munair <munair@qe.local>
2016-05-02 13:59:23 +0900

    add remaining glyicons

--- templates/navigation.php
+++ templates/navigation.php
@@ -9,10 +9,10 @@
         </div>
         <nav>
             <ul>
-                <li><a href="#about"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span>About</a></li>
-                <li><a href="#introduction">Introduction</a></li>
-                <li><a href="#artists">Artists</a></li>
-                <li><a href="#portfolios">Portfolios</a></li>
+                <li><a href="#about"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> / About</a></li>
+                <li><a href="#introduction"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> / Introduction</a></li>
+                <li><a href="#artists"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> / Artists</a></li>
+                <li><a href="#portfolios"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> / Portfolios</a></li>
             </ul>
         </nav>
     </div> <!-- / row -->
