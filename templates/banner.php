d832c9e57d2a227ea9a30b6da892f1e77bd3e7c6
munair <munair@qe.local>
2016-05-02 23:40:55 +0900

    new div test

--- templates/banner.php
+++ templates/banner.php
@@ -5,6 +5,7 @@
 		<li></li>
 		<li></li>
 	</ul>
+	<div class="notice"><p>this is block text</p></div>
 	<div class="v-center">
 		<a onmousedown="tick()" onmouseover="pop()" href="#ribbon">
 			<img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/faded-handshaker-logo-1000x1000px.png" />
