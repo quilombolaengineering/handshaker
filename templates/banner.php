97af7f4dc818727a73e40f5c5027618e61f3487d
munair <munair@qe.local>
2016-05-02 18:58:41 +0900

    revert

--- templates/banner.php
+++ templates/banner.php
@@ -1,7 +1,7 @@
 <section id="banner" class="section-banner">
 	<!-- slider animation -->
 	<ul class="keyframe-slider">
-		<li>The Decorated Projects section above is a collection of art that has won awards. If you would like to collaborate with us as an artist-in-residence, please review Upcoming Projects. It is essentially a call for entry / call for submission. The curatorial plans submitted by artists like yourself are presented there. Art projects that we are presently working on are cataloged in Current Projects. Finally, all of our completed artwork has been categorized under Completed Projects.</li>
+		<li></li>
 		<li></li>
 		<li></li>
 	</ul>
