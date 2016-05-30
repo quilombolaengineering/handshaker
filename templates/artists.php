945cfbc2295030703c254c89d501038d9f2d114f
munair <munair@qe.local>
2016-05-31 01:43:19 +0900

    reformat

--- templates/artists.php
+++ templates/artists.php
@@ -16,37 +16,32 @@
 			<div class="row">
                 		<div class="col-md-2 col-md-offset-1">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dude-2000x2000px.png" /></a>
-					<h1>Dude</h1>
+					<h3>Dude</h3>
 					<p>Marketing & Content Development</p>
-					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
                 		<div class="col-md-2">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
-					<h1>Kai Qin</h1>
+					<h3>Kai Qin</h3>
 					<p>Value Financing & Art Curation</p>
-					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
 				<div class="col-md-2">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/duderoni-2000x2000px.png" /></a>
-					<h1>Duderoni</h1>
+					<h3>Duderoni</h3>
 					<p>Portfolio & Graphic Management</p>
-					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
 				<div class="col-md-2">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dudette-2000x2000px.png" /></a>
-					<h1>Dudette</h1>
+					<h3>Dudette</h3>
 					<p>Portfolio & Graphic Management</p>
-					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
 				<div class="col-md-2">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/mary-2000x2000px.png" /></a>
-					<h1>Mary</h1>
+					<h3>Mary</h3>
 					<p>Portfolio & Graphic Management</p>
-					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
 			</div>
