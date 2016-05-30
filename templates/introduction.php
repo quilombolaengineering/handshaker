2ca1eb0f55289850bc54d59cc91c4ae4e8ab41a8
munair <munair@qe.local>
2016-05-31 00:00:55 +0900

    updating the introduction and gitignore

--- templates/introduction.php
+++ templates/introduction.php
@@ -23,14 +23,14 @@
 	<div id="introduction" class="container-fluid organization-story">
 		<div class="row">
 			<div class="col-xs-12">
-<h1>Introduction</h1>
-<p>This is a compelling story about Handshaker and its mission to change the world. In this section I will make you fall in love with Handshaker. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i>. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. </p>
-
-<p>Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <b>Maecenas mattis</b>. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. <i>Aenean quam</i>. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. </p>
-
-<p><b>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh</b>. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. <b>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos</b>. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. </p>
-
-<p>Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. <i>Quisque volutpat condimentum velit</i>. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. <i>Sed non quam</i>. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. </p>
+<h1>握手302</h1>
+<p>“握手302”是一间位于白石洲上白石村二坊49栋302号的出租屋。城中村里有着惊人的建筑密度，人们把这些以出租为目的的村民自建房屋戏称为“握手楼”。这间12.5平米的小房间位于本栋 “握手楼”的第三层，包括一个房间和一个洗手间，这样的房间一层共有十间。</p>
+<p>这是一个独立的艺术空间，有一个5人的核心团队负责策划和日常工作，但通过公开的通知，任何人都能参与到这里的艺术实践和交流里来。项目的参与者们会在这里工作甚至居住。从2013年10月开始至今，握手302已经在九个不同的主题之下举办了超过五十次系列活动。</p>
+<p>我们相信：艺术属于每一个为我们城市做出贡献的人</p>
+<h1>Handshake 302</h1>
+<p>"Handshake 302" is located at 302, Building 49, Shangbaishi Block 2 in Baishizhou. "Handshake" buldings stand so closely together that it is possible to shake hands through the windows of adjacent buildings. In fact, “Handshake 302” is a repurposed, 12 m2 efficiency apartment on the third floor of a stereotypic “handshake building”. It has a kitchen and a bathroom. There are 10 rooms on this floor.</p>
+<p>Handshake 302 is an independent art space. A core group of five people curates the space, but participation in any project is open to anyone through public announcements. Project participation ranges from using the space as an exhibition space to living quarters. Since its opening in October 2013, Handshake 302 has curated six exhibitions and more than 50 public events.</p>
+<p>We believe that art belongs to everyone who contributes to our city.</p>
 			<div class="yellow-button">Read More</div>
 			</div>
 		</div>
