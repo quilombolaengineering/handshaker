5fc99550d45d9072374304cd6faddbef1beccc41
munair <munair@qe.local>
2016-05-31 00:48:35 +0900

    add introduction artists

--- templates/artists.php
+++ templates/artists.php
@@ -12,23 +12,37 @@
 					<h1>Principal Artists</h1>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 				</div>
-                		<div class="col-md-4">
-					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/headshot-zumbi-441x441px.png" /></a>
-					<h1>Zumbi</h1>
+                		<div class="col-md-2 col-md-offset-1">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dude-2000x2000px.png" /></a>
+					<h1>Dude</h1>
 					<p>Marketing & Content Development</p>
 					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
-                		<div class="col-md-4">
+                		<div class="col-md-2">
 					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
 					<h1>Kai Qin</h1>
 					<p>Financing & Art Curation</p>
 					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
 				</div>
-				<div class="col-md-4">
-					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/soyoon-1000x1000px.png" /></a>
-					<h1>Soyoon</h1>
+				<div class="col-md-2">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/duderoni-2000x2000px.png" /></a>
+					<h1>Duderoni</h1>
+					<p>Portfolio & Graphic Management</p>
+					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<div class="yellow-button">More Info</div>
+				</div>
+				<div class="col-md-2">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dudette-2000x2000px.png" /></a>
+					<h1>Dudette</h1>
+					<p>Portfolio & Graphic Management</p>
+					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					<div class="yellow-button">More Info</div>
+				</div>
+				<div class="col-md-2">
+					<a href="#ribbon"><img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/mary-2000x2000px.png" /></a>
+					<h1>Mary</h1>
 					<p>Portfolio & Graphic Management</p>
 					<p class="contributor-intro">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 					<div class="yellow-button">More Info</div>
