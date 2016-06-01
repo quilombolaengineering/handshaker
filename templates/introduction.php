3b3b243a1d2985e33751580d9a8e804d249c7ef1
munair <munair@qe.local>
2016-06-01 22:30:56 +0900

    restructuring the about section

--- templates/introduction.php
+++ templates/introduction.php
@@ -3,11 +3,11 @@
 	<div class="container-fluid organization-summary">
 		<div class="v-center">
 			<!-- Introduction Image -->
-			<div class="col-xs-12 col-md-6 introduction-image"><div class="logoReveal"><img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-logo.jpg"></div></div>
+			<div class="col-md-2 col-md-offset-5"><div class="logoReveal"><img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-logo.jpg"></div></div>
 			<!-- Introduction Text -->
-			<div class="col-xs-12 col-md-6 introduction-title"><div class="titleReveal"><h2><?php bloginfo('name'); ?></h2></div></div>
-			<div class="col-xs-12 col-md-6 introduction-line"><div class="colored-line"></div></div>
-			<div class="col-xs-12 col-md-6 introduction-text"><div class="aboutReveal"><?php bloginfo('name'); ?>
+			<div class="col-md-2 col-md-offset-5"><div class="titleReveal"><h2><?php bloginfo('name'); ?></h2></div></div>
+			<div class="col-md-2 col-md-offset-5"><div class="colored-line"></div></div>
+			<div class="col-md-2 col-md-offset-5"><div class="aboutReveal"><?php bloginfo('name'); ?>
 							希望通过这个网站向大家简易，清晰地介绍握手302的情况，也能邀请感兴趣的朋友共同参与</div></div>
 			</div>
 		</div>
