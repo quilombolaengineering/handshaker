53a8ac1ed30fc22a84268d1979575f1b627dfecf
munair <munair@qe.local>
2016-06-01 17:52:16 +0900

    add logo

--- templates/introduction.php
+++ templates/introduction.php
@@ -6,7 +6,7 @@
 			<div class="col-xs-4 col-md-3 col-md-offset-3">
 				<div class="logoContainer">
 					<div class="logoReveal"> 
-						<img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/cochin-q-circle-orangeicon-center-1000x1000px.png">
+						<img class="introduction-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-logo.jpg">
 					</div>
 				</div>
 			</div>
