3a660c6f3a6e8dd71d860201da577a16e6d4ad34
munair <munair@qe.local>
2016-06-03 11:31:00 +0900

    swap out favicon

--- templates/head.php
+++ templates/head.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/favicon.ico" />
+  <link rel="shortcut icon" href="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-zumba-icon-600x600px.png" />
   <script src="https://cdn.jsdelivr.net/scrollreveal.js/3.1.4/scrollreveal.min.js"></script>
   <script>
       window.sr = ScrollReveal();
