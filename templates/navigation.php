f5c7aa0ab28d853b831fe93bb08e14ed4ffce3fe
munair <munair@qe.local>
2016-06-03 09:31:35 +0900

    update navigation

--- templates/navigation.php
+++ templates/navigation.php
@@ -9,10 +9,9 @@
         </div>
         <nav>
             <ul>
-                <li><a href="#about" style="text-decoration: none;"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> About</a></li>
-                <li><a href="#introduction" style="text-decoration: none;"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span> Introduction</a></li>
-                <li><a href="#artists" style="text-decoration: none;"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Artists</a></li>
-                <li><a href="#portfolios" style="text-decoration: none;"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Portfolios</a></li>
+                <li><a href="#about" style="text-decoration: none;"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span> 关于艺术家</a></li>
+                <li><a href="#artists" style="text-decoration: none;"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 艺术家</a></li>
+                <li><a href="#portfolios" style="text-decoration: none;"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> 作品集</a></li>
             </ul>
         </nav>
     </div> <!-- end row -->
