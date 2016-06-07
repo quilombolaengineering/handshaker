88df5be1225800b8a00e543c4ccb33ceca6ad2df
munair <munair@qe.local>
2016-06-07 18:24:45 +0900

    add category title

--- templates/archive-header.php
+++ templates/archive-header.php
@@ -10,7 +10,8 @@
                         <img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-zumba-icon-600x600px.png" />
                 </a>
                 <h1><?php bloginfo('name'); ?></h1>
-	    	<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<p><?php single_cat_title('Currently browsing '); ?>.</p>
+	    	<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 		<nav class="nav-primary">
 			<?php
 				if (has_nav_menu('primary_navigation')) :
