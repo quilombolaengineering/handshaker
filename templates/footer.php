0b112cf44f475cfbe5a61e4022d9135d29ba8b38
munair <munair@qe.local>
2016-06-08 14:19:53 +0900

    developed by quilombola enginerring

--- templates/footer.php
+++ templates/footer.php
@@ -29,7 +29,7 @@
 		</div>
 		<div class="row">
 		        <div class="col-md-4 col-md-offset-4">
-		                <p class="about-text">Maintained by <a href="http://www.quilombolaengineering.com">Quilombola Engineering</a>. </p>
+		                <p class="about-text">Developed by <a href="http://www.quilombolaengineering.com">Quilombola Engineering</a>. </p>
 	        	</div>
 		</div>
 	</div>
