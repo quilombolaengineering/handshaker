e0638a7d6aaf816278ceacb9f2c98a4db2e870bf
munair <munair@qe.local>
2016-06-08 18:28:27 +0900

    add standard header

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,22 @@
-<header class="header">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
-  </div>
-</header>
+                <header class="header">
+                        <!-- slider animation -->
+                        <ul class="keyframe-fader">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                        </ul>
+                        <div class="v-center">
+                                <a onmousedown="tick()" onmouseover="pop()" href="<?= esc_url(home_url('/')); ?>">
+                                        <img class="scroll-down" src="https://s3-ap-northeast-1.amazonaws.com/www-capoeirastudio-com/capoeirastudio-whitealpha-icon-600x600px.png" />
+                                </a>
+                                <h1><?php bloginfo('name'); ?></h1>
+                                <h4 class="entry-title"><?php the_title(); ?></h4>
+                                <nav class="nav-primary">
+                                        <?php
+                                                if (has_nav_menu('primary_navigation')) :
+                                                        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+                                                endif;
+                                        ?>
+                                </nav>
+                        </div>
+                </header>
