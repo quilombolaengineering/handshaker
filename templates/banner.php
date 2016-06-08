a4cdf42b17602bdccc88bdecdd091681b3e51762
munair <munair@qe.local>
2016-06-08 12:42:42 +0900

    add animation to banner

--- templates/banner.php
+++ templates/banner.php
@@ -7,7 +7,7 @@
 	</ul>
 	<div class="v-center">
 		<a onmousedown="tick()" onmouseover="pop()" href="#portfolios">
-			<img class="scroll-down" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-zumba-icon-600x600px.png" />
+			<img class="scroll-down wow rotateIn" wow-data-delay="2s" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-zumba-icon-600x600px.png" />
 		</a>
 		<h1><?php bloginfo('name'); ?></h1> 
 		<p><?php bloginfo('description'); ?></p>	
