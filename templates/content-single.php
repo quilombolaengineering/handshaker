8fa07c3408c2d5026c7f11c33ad62f30473220fe
munair <munair@qe.local>
2016-06-09 00:10:53 +0900

    format content single

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,11 +1,18 @@
-<?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
-<?php endwhile; ?>
+<section id="posts" class="section-posts">
+        <!-- Post Content-->
+        <div id="main" class="container-fluid">
+                <div class="row">
+			<?php while (have_posts()) : the_post(); ?>
+			  <article <?php post_class(); ?>>
+			    <div class="entry-content">
+			      <?php the_content(); ?>
+			    </div>
+			    <footer>
+			      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+			    </footer>
+			    <?php comments_template('/templates/comments.php'); ?>
+			  </article>
+			<?php endwhile; ?>
+		</div>
+	</div>
+</section>
