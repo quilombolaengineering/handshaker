04183b250e8c2294194fad03132130bea0b89e1c
munair <munair@qe.local>
2016-06-09 00:21:21 +0900

    go to bed

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,17 +2,19 @@
         <!-- Post Content-->
         <div id="main" class="container-fluid">
                 <div class="row">
-			<?php while (have_posts()) : the_post(); ?>
-			  <article <?php post_class(); ?>>
-			    <div class="entry-content">
-			      <?php the_content(); ?>
-			    </div>
-			    <footer>
-			      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-			    </footer>
-			    <?php comments_template('/templates/comments.php'); ?>
-			  </article>
-			<?php endwhile; ?>
+                	<div class="col-xs-10 col-xs-offset-1">
+				<?php while (have_posts()) : the_post(); ?>
+				  <article <?php post_class(); ?>>
+				    <div class="entry-content">
+				      <?php the_content(); ?>
+				    </div>
+				    <footer>
+				      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+				    </footer>
+				    <?php comments_template('/templates/comments.php'); ?>
+				  </article>
+				<?php endwhile; ?>
+			</div>
 		</div>
 	</div>
 </section>
