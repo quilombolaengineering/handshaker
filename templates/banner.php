42f367f0b9a8437a9bc5aefa054393ff2db7ae14
munair <munair@qe.local>
2016-06-08 15:49:32 +0900

    add animation options

--- templates/banner.php
+++ templates/banner.php
@@ -7,10 +7,9 @@
 	</ul>
 	<div class="v-center">
 		<a onmousedown="tick()" onmouseover="pop()" href="#portfolios">
-			<img class="animated rotateIn" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-zumba-icon-600x600px.png" />
+			<img class="animated rotateIn options" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/handshake302-zumba-icon-600x600px.png" />
 		</a>
 		<h1><?php bloginfo('name'); ?></h1> 
 		<p><?php bloginfo('description'); ?></p>	
-		<!-- let's leave out the yellow button div <div class="yellow-button">Usage</div> -->
 	</div>
 </section>
