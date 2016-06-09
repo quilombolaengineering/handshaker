86b4b5e1518c83558058ff165edcec2d3a319be1
munair <munair@qe.local>
2016-06-09 11:56:59 +0900

    tidy up php

--- templates/archive-header.php
+++ templates/archive-header.php
@@ -11,18 +11,16 @@
                 </a>
                 <h1><?php bloginfo('name'); ?></h1>
 		<p><?php single_cat_title('Currently browsing the '); ?> category.</p>
-		<p><?php echo "Current Category ID is: " . $cat ;?></p>
+		<?php
+			$args = array( 'posts_per_page' => 5, 'offset'=> 0, 'category' => $cat );
+			$myposts = get_posts( $args );
 
-<?php
-
-$current_category = single_cat_title("", false);
-$args = array( 'posts_per_page' => 5, 'offset'=> 0, 'category' => $cat );
-
-$myposts = get_posts( $args );
-foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
-		<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-<?php endforeach; 
-wp_reset_postdata();?>
+			foreach ( $myposts as $post ) : setup_postdata( $post ); 
+		?>
+			<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		<?php endforeach; 
+			wp_reset_postdata();
+		?>
 		<nav class="nav-primary">
 			<?php
 				if (has_nav_menu('primary_navigation')) :
