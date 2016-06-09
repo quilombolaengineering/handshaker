628201c63811e98084a6925450a234b91586514f
munair <munair@qe.local>
2016-06-09 23:33:47 +0900

    remove errors

--- assets/scripts/social-icon-rotate-in.js
+++ assets/scripts/social-icon-rotate-in.js
@@ -2,7 +2,6 @@
  * ScrollReveal Animations - Animate element using scrollReveal.js
  */
 
-<!-- make reveal calls last -->
 // use rotation in reveal configuration
 var socialIcon= { origin : 'right', distance : '25vw', duration : 2000, easing : 'ease-out' };
 
