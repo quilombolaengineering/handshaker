c6b930ff20f12f47b1d6733153199ae032b307c9
munair <munair@qe.local>
2016-06-09 13:14:44 +0900

    update artist text styling

--- templates/artists.php
+++ templates/artists.php
@@ -17,27 +17,27 @@
                 		<div class="col-md-2 col-md-offset-1">
 					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dude-2000x2000px.png" /></a>
 					<h3>刘赫 </h3>
-					<p>Liu He</p>
+					<h2>Liu He</h2>
 				</div>
                 		<div class="col-md-2">
 					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
 					<h3>张凯琴 </h3>
-					<p>Zhang Kai Qin</p>
+					<h2>Zhang Kai Qin</h2>
 				</div>
 				<div class="col-md-2">
 					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/duderoni-2000x2000px.png" /></a>
 					<h3>雷胜</h3>
-					<p>Lei Sheng</p>
+					<h2>Lei Sheng</h2>
 				</div>
 				<div class="col-md-2">
 					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dudette-2000x2000px.png" /></a>
 					<h3>吴丹</h3>
-					<p>Wu Dan</p>
+					<h2>Wu Dan</h2>
 				</div>
 				<div class="col-md-2">
 					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/mary-2000x2000px.png" /></a>
 					<h3>马立安</h3>
-					<p>Marry Ann O'Donnell</p>
+					<h2>Marry Ann O'Donnell</h2>
 				</div>
 			</div>
 		</div>
