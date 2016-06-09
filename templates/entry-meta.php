27f3a440b72cb47616c7eb0c90104f6f60c234b8
munair <munair@qe.local>
2016-06-09 12:24:43 +0900

    add icon

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,1 +1,1 @@
-<time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+<time class="updated" datetime="<?= get_post_time('c', true); ?>"><i class="glyphicon glyphicon-calendar"></i> <?= get_the_date(); ?></time>
