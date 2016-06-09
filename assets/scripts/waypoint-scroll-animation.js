4ece0877dc9d466a142a06bf175cb2b856ed39d9
munair <munair@qe.local>
2016-06-10 00:11:58 +0900

    45%

--- assets/scripts/waypoint-scroll-animation.js
+++ assets/scripts/waypoint-scroll-animation.js
@@ -10,6 +10,6 @@ $(document).ready(function(){
  
   $('#facebookIcon').waypoint(function() {
       $('#facebookIcon').addClass('rotateIn');
-  }, { offset: '5%' });
+  }, { offset: '45%' });
  
 });
