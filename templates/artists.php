32999d1cc747bbd516033c66eecbf4efebba4845
munair <munair@qe.local>
2016-06-20 15:27:37 +0900

    anticipate changes

--- templates/artists.php
+++ templates/artists.php
@@ -15,17 +15,17 @@
 			</div>
 			<div class="row">
                 		<div class="col-md-2 col-md-offset-1">
-					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dude-2000x2000px.png" /></a>
+					<a href="http://handshake302.com/index.php/founder-liuhe/"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/dude-2000x2000px.png" /></a>
 					<h3>刘赫 </h3>
 					<h2>Liu He</h2>
 				</div>
                 		<div class="col-md-2">
-					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
+					<a href="http://handshake302.com/index.php/founder-zhang-kaiqin/"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/handshaker-theme/zhangkaiqin-train-600x600px.png" /></a>
 					<h3>张凯琴 </h3>
 					<h2>Zhang Kai Qin</h2>
 				</div>
 				<div class="col-md-2">
-					<a href="#ribbon"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/duderoni-2000x2000px.png" /></a>
+					<a href="http://handshake302.com/index.php/founder-leisheng/"><img class="artist-image" src="https://s3.ap-northeast-2.amazonaws.com/www-handshake302-com/duderoni-2000x2000px.png" /></a>
 					<h3>雷胜</h3>
 					<h2>Lei Sheng</h2>
 				</div>
